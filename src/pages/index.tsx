2a851b7784a926324e96ed6e9b3822dff48a5849
metfor <mateus_frade@hotmail.com>
2021-05-31 12:51:22 -0300

    pt1 1/3

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+      <title>IgNews | Inicio</title>
+    </Head>
+    <h1 >salve
+    
+    </h1>
+    </>
+  )
+}
