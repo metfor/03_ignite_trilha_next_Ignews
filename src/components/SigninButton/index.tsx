6cdd5dfca6093f04551fdb8f87b90f412f9bb402
metfor <mateus_frade@hotmail.com>
2021-06-01 16:10:26 -0300

    pt2 1/4

--- src/components/SigninButton/index.tsx
+++ src/components/SigninButton/index.tsx
@@ -1,16 +1,22 @@
+import {signIn,useSession,signOut} from "next-auth/client"
 import styles from "./styles.module.scss"
 import {FaGithub} from "react-icons/fa"
 import{FiX}from "react-icons/fi"
 export function SigninButton(){
-    const isUserLoggedIn= true;
-    return isUserLoggedIn ?(
-        <button type="button" className={styles.signInButton}>
+    const [session]=useSession()
+    return session ?(
+        <button type="button" className={styles.signInButton} onClick={()=>signOut()}
+        >
             <FaGithub color="#04d361"/>
-            Matheus Frade
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon}/>
         </button>
     ):(
-        <button type="button" className={styles.signInButton}>
+        <button 
+            type="button" 
+            className={styles.signInButton}
+            onClick={()=>signIn("github")}
+        >
             <FaGithub color="#eba417"/>
             Sign in with Github
             
