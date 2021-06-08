ae4c4b8885638cf4852548b9a97371ede8f520d7
metfor <mateus_frade@hotmail.com>
2021-06-08 10:09:36 -0300

    pt3 1/2

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,18 @@
+
 import { SigninButton } from "../SigninButton"
 import styles from "./styles.module.scss"
+import React from "react"
+import { ActiveLink} from "../ActiveLink"
 export function Header (){
+    
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="logo"/>
                 <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
+                    <ActiveLink href="/" activeClassName={styles.active}><a>Home</a></ActiveLink>
+                    <ActiveLink href="/posts" activeClassName={styles.active} prefetch><a>Posts</a></ActiveLink>
+                    
                 </nav>
                 <SigninButton/>
             </div>
